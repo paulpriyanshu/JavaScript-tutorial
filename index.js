1c8b0ec2a5bf66bdb799e774407d7b0eb4413c73
paulpriyanshu <priyanshu.paul003@gmail.com>
2024-02-02 00:33:02 +0530

    first teachings

---
+++ index.js
@@ -0,0 +1,39 @@
+// let a=10
+// //const a=10
+// a=5
+// //console.log(a)
+// for(var i=0;i<5;i++){
+//   console.log(i)
+// }
+// console.log(i)
+
+console.log(sum(5,4))
+
+// let arr1=[1,2]
+// let arr=[
+//     {
+//           name: "Rahul",
+//           age: 20
+//         },
+//          {
+//            name:"ybbi",
+//            age:18
+//          }
+//         ]
+// let str="20"
+// let num=10
+// console.log(str+num)
+function sum(a,b){
+  let ans=a+b
+  return ans
+
+}
+
+
+
+
+// // console.log(arr[1])
+// // console.log(arr1[0])
+
+
+
