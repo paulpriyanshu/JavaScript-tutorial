fc6576cd4dc9e00f7478956c601356a4f95a60d1
paulpriyanshu <priyanshu.paul003@gmail.com>
2024-02-04 19:19:41 +0530

    day2done

--- Day2.js
+++ Day2.js
@@ -1,3 +1,64 @@
-const newfunc=()=>{
-    console.log("hello done")
+// //========callback function
+// // function callback(data) {
+// //     console.log(data);
+// // }   
+// // //========
+
+const example2=()=>{
+    console.log(`hello this is a call back example`);
+
 }
+
+
+// function username(()=>{
+//     console.log("yadvi")
+// })
+
+// const message = function() {  
+//     console.log("This message is shown after 3 seconds");
+// }
+ 
+// setTimeout(()=>{
+//     console.log("This message is shown after 3 seconds")
+// }, 3000);
+
+// function example(()=>{
+//     console.log("hello")
+// })
+
+// const example=()=>{
+//     console.log("hello")
+// }
+// example()
+//================================================================asynchronous/synchronous========================
+
+// const timer=()=>{
+   
+//     setTimeout(()=>{
+//         console.log("hello i'm async")
+//     },2000)
+    
+// }
+// const print=()=>{
+//     console.log("hello i'm sync")
+// }
+// print()
+// timer()
+// console.log("hello i'm running")
+
+// function delay_Greeting() {
+//     setTimeout(function async() {
+//       console.log("Hello, Ninja!");
+//     }, 5000); // Wait for 5000 milliseconds (5 seconds)
+//    }
+   
+//    console.log("Start");
+//    delay_Greeting();
+//    console.log("End");
+
+   const name = "John"
+const age = 25
+
+const greeting = name === "Jon" ? "Hello" : age <25 ? "Sir" : "Ma'am"
+
+console.log(greeting)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const example2=()=>{
+    console.log(`hello this is a call back example`);
+
+}
